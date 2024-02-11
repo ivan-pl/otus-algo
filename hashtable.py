90c6e4356129a940ea033c106ade4cb048a29aeb
ivan-pl <pleskaiv@gmail.com>
2024-02-11 20:32:21 +0300

    feat: add hash function

--- hashtable.py
+++ hashtable.py
@@ -12,7 +12,12 @@ LinkedList: TypeAlias = list[Node]
 
 class HashTable():
     _load_factor = 0.75
+    _table_length = 10
+    _cells_occupied = 0
     _values = np.array(10, dtype=object)
+
+    def _get_hash(self, item: Any) -> int:
+        return hash(item) % self._table_length
 
     def get(self):
         pass
