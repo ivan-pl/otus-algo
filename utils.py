be945f69d48874019fcfefc08c2bcf6bffaefd65
ivan-pl <pleskaiv@gmail.com>
2024-02-12 16:53:22 +0300

    feat: add generate_file

--- utils.py
+++ utils.py
@@ -1,13 +1,14 @@
 from numpy import random, arange
 import time
 from typing import Callable, Sequence, Any
+import os
 
 
 def timeit_sort(size: int, sort_func: Callable[[Sequence[int]], Sequence[Any]], max_random_value: int = 100_000,
                 seed: int = 49) -> None:
-    random.seed(seed)
+    r = random.RandomState(seed)
 
-    array = random.randint(max_random_value, size=size)
+    array = r.randint(max_random_value, size=size)
     start_time = time.time()
     sort_func(array)
     print(f'{sort_func.__name__} RANDOM size={size!r} time: {time.time() - start_time:.3f} seconds')
@@ -21,3 +22,13 @@ def timeit_sort(size: int, sort_func: Callable[[Sequence[int]], Sequence[Any]], 
     start_time = time.time()
     sort_func(array)
     print(f'{sort_func.__name__} REVERSED size={size!r} time: {time.time() - start_time:.3f} seconds')
+
+
+def generate_file(row_count: int, max_number: int, seed: int = 49) -> str:
+    r = random.RandomState(seed)
+    path = os.path.join('generated_files', f'{row_count}_rows_{max_number}_max.txt')
+    with open(path, 'w') as f:
+        for i in range(row_count):
+            f.write(f'{r.randint(1, max_number)}\n')
+
+    return path
